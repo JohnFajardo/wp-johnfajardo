7c71727e9781927403b06338765f909878d5bfd5
ThatJohn <fajardocj@gmail.com>
2020-02-23 23:32:13 -0500

    Reverts to no pagination due to bug

--- footer.php
+++ footer.php
@@ -8,24 +8,3 @@
         </ul>
 
         <hr />
-	
-<?php $args = array(
-    'base'               => '%_%',
-    'format'             => '?paged=%#%',
-    'total'              => 1,
-    'current'            => 0,
-    'show_all'           => true,
-    'end_size'           => 1,
-    'mid_size'           => 2,
-    'prev_next'          => true,
-    'prev_text'          => __('« Previous'),
-    'next_text'          => __('Next »'),
-    'type'               => 'plain',
-    'add_args'           => true,
-    'add_fragment'       => '',
-    'before_page_number' => '',
-    'after_page_number'  => ''
-); 
-get_the_posts_pagination($args);
-
-?>
