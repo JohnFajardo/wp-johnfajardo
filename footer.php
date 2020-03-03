9d0449ce63effefe2c5f7f8b92f9b93d6f5c03e3
ThatJohn <fajardocj@gmail.com>
2020-03-03 08:40:58 -0500

    Cleans up footer code

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
-        <ul class="pagination">
-          <li class="prev-page"><a href="#"><</a></li>
-          <li class="page"><a href="#">1</a></li>
-          <li class="page"><a href="#">2</a></li>
-          <li class="active page">3</li>
-          <li class="page"><a href="#">4</a></li>
-          <li class="next-page"><a href="#"">></a></li>
-        </ul>
-
-        <hr />
+<ul class="pagination">
+  <li class="prev-page"><a href="#"><</a></li>
+  <li class="page"><a href="#">1</a></li>
+  <li class="page"><a href="#">2</a></li>
+  <li class="active page">3</li>
+  <li class="page"><a href="#">4</a></li>
+  <li class="next-page"><a href="#"">></a></li>
+</ul>
