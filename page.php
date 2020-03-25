ce891785ee027d7460dbd15feff14bd9b01cd4c5
ThatJohn <fajardocj@gmail.com>
2020-03-24 20:28:09 -0400

    Adds single page template

--- page.php
+++ page.php
@@ -4,14 +4,10 @@
     <?php get_sidebar(); ?>
 
     <div class="content">
-        <h2 class="latest-posts">Latest posts</h2>
-        <hr />
-
+        
         <?php while ( have_posts() ) : the_post(); ?>
 
         <article>
-            <h3 class="post-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <p class="metadata"><span class="icon-calendar"></span> <?php the_date();?> -  <span class="icon-user"> </span>  <?php the_author();?></p>
             <?php the_content(); ?>
         </article>
         
