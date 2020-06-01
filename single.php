27eaeabf09ba469a32e0b09ca467b38248efaea6
ThatJohn <fajardocj@gmail.com>
2020-06-01 05:05:20 -0400

    Adds additional mobile styles

--- single.php
+++ single.php
@@ -6,11 +6,13 @@
     <div class="content main-content">
 
     
+    <div class="post-content">
         <?php
             while ( have_posts() ) : the_post();
                 get_template_part( 'content', 'single' );
             endwhile;
         ?>
+    </div>
 
 
     </div><!--  End content -->
