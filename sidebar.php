c897ed4ec89d41baf1263091ea7aae35c714e584
ThatJohn <fajardocj@gmail.com>
2020-06-02 00:14:23 -0400

    Adds further mobile styles

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div class="navigation">
         <a href="<?php echo get_bloginfo('wpurl'); ?>" class="home-link"><img class="profile" src="<?php echo get_template_directory_uri(); ?>/images/profile.png" alt=""></a>
         <h1 class="title"><a href="<?php echo get_bloginfo('wpurl'); ?>">John Fajardo</a></h1>
-        <p class="tagline">Full stack web developer</p>
+        <p class="tagline">Software engineer</p>
         <nav>
             <!-- Walker -->
             <?php
